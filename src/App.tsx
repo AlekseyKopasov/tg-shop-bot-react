a30239adc60efd4097e40e9f341194416b83668e
Aleksey Kopasov <alesey.k1@gmail.com>
2022-10-21 23:51:14 +0300

    {feat}: добавляет telegram web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
-function App() {
+let tg =  window.Telegram.WebApp
+
+const App: React.FC = () => {
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+
   return (
     <div className="App">
-      New project
+      <button onClick={onClose}>Закрыть</button>
     </div>
   )
 }
