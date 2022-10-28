772e376844e09129c815938ec9e966a652cb7366
Aleksey Kopasov <alesey.k1@gmail.com>
2022-10-29 00:50:47 +0300

    {feat}: получает данные от бота

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -16,7 +16,7 @@ const Form: React.FC = () => {
     }
 
     tg.sendData(JSON.stringify(data))
-  }, [])
+  }, [country, street, subject])
 
   useEffect(() => {
     tg.onEvent('backButtonClicked', onSendData)
