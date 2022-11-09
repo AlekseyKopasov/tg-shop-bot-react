2a38cf25b9ce8f84d00c14eea53556d9b32e9d07
Aleksey Kopasov <alesey.k1@gmail.com>
2022-11-10 00:42:29 +0300

    {feat}: добавляет корзину товаров

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -36,7 +36,7 @@ const ProductList: React.FC = () => {
       queryId
     }
 
-    fetch('http://localhost:8000', {
+    fetch('http://localhost:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
